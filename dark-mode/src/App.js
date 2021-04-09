85e25772bbe09f0dcc0f145fae371711f3a8ff35
Clemence Gazier <clemencegazier@MacBook-Pro-de-Clemence.local>
2021-04-09 12:03:44 +0200

    add styled components

--- dark-mode/src/App.js
+++ dark-mode/src/App.js
@@ -1,10 +1,16 @@
 import { Content } from './components/Content';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  max-width: 50%;
+  margin: 8rem auto 0;
+`;
 
 function App() {
   return (
-    <div className="App">
+    <Container>
       <Content />
-    </div>
+    </Container>
   );
 }
 
